785018b12f852bf2daf3212226dd7c6f61443ee1
Will <yzj_000@163.com>
2025-12-23 20:29:15 +0800

    Initial commit

---
+++ src/main/java/io/will/springbootv4poc/SpringBootV4PocApplication.java
@@ -0,0 +1,13 @@
+package io.will.springbootv4poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootV4PocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootV4PocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/will/springbootv4poc/controller/PingControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package io.will.springbootv4poc.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
+class PingControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private RestTestClient restTestClient;
+
+    @Test
+    void testPing_shouldReturnPong() {
+        restTestClient.get()
+                .uri("http://localhost:" + port + "/ping")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .isEqualTo("pong");
+    }
+}
+
